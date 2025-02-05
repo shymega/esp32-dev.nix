edb9236d1eb4792466b592b4ccdbe58df4aa9e81
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-05 21:52:43 +0000

    refactor: Migrate to Snowfall, and add openocd-esp32 package
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    unstable.url = "nixpkgs/nixpkgs-unstable";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    snowfall-drift = {
+      url = "github:snowfallorg/drift";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
@@ -8,91 +17,18 @@
     };
     crane.url = "github:ipetkov/crane";
   };
-  outputs = { self, ... }@inputs:
-    inputs.flake-utils.lib.eachSystem [ "x86_64-linux" ]
-      (system:
-        let
-          pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [ inputs.fenix.overlays.default ];
-            config = {
-              allowBroken = true;
-            };
-          };
-          inherit (pkgs.stdenv) mkDerivation;
-          inherit (pkgs) fetchzip;
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
 
-          rust-esp-version = "1.80.0.0";
+      overlays = with inputs; [
+        snowfall-drift.overlays.default
+        fenix.overlays.default
+      ];
 
-          rust-esp-toolchain-src = fetchzip {
-            url = "https://github.com/esp-rs/rust-build/releases/download/v${rust-esp-version}/rust-src-${rust-esp-version}.tar.xz";
-            sha256 = "sha256-mnPUNyB1w1lqrSfpcnZi5OO/l84CuuidbP6TEMKMBV8=";
-          };
-
-          rust-esp-toolchain-bin = fetchzip {
-            url = "https://github.com/esp-rs/rust-build/releases/download/v${rust-esp-version}/rust-${rust-esp-version}-x86_64-unknown-linux-gnu.tar.xz";
-            sha256 = "sha256-y07tXCTlzAN22O950U89jiBFsou+K9DiiIy5oCYqlV8=";
-          };
-
-          esp-toolchain = mkDerivation {
-            name = "esp-toolchain";
-
-            # Skip src requirement
-            unpackPhase = "true";
-
-            buildInputs = [
-              rust-esp-toolchain-bin
-              rust-esp-toolchain-src
-
-              # make it work on NixOS
-              pkgs.autoPatchelfHook
-              pkgs.stdenv.cc.cc.lib
-              pkgs.zlib
-            ];
-
-            installPhase = ''
-              mkdir -p $out/
-              mkdir -p $out/lib/rustlib
-
-              # somehow otherwise has no permission to copy
-              touch $out/lib/rustlib/uninstall.sh
-
-              bash ${rust-esp-toolchain-bin}/install.sh --destdir=$out --prefix="" --without=rust-docs-json-preview,rust-docs --disable-ldconfig
-              bash ${rust-esp-toolchain-src}/install.sh --destdir=$out --prefix="" --disable-ldconfig
-            '';
-          };
-
-
-          xtensa-esp32-elf-clang = mkDerivation {
-            name = "xtensa-esp32-elf-clang";
-
-            src = fetchzip {
-              url = "https://github.com/espressif/llvm-project/releases/download/esp-16.0.0-20230516/libs_llvm-esp-16.0.0-20230516-linux-amd64.tar.xz";
-              sha256 = "sha256-T9MBRQZ5fI+x3SuDL1ngd0ksq+VdZ5DMnE6VROxu85c=";
-            };
-
-            buildInputs = [
-              # make it work on NixOS
-              pkgs.autoPatchelfHook
-              pkgs.stdenv.cc.cc.lib
-              pkgs.zlib
-              pkgs.libxml2
-            ];
-
-            installPhase = ''
-              cp -R $src $out
-            '';
-          };
-
-        in
-        rec {
-          packages = {
-            inherit esp-toolchain xtensa-esp32-elf-clang;
-            allPackages = packages.esp-toolchain // packages.xtensa-esp32-elf-clang;
-          };
-        }) // {
-      overlays.default = final: prev: {
-        inherit (self.packages.${final.system}) esp-toolchain xtensa-esp32-elf-clang;
+      outputs-builder = channels: {
+        formatter = channels.nixpkgs.alejandra;
       };
     };
 }
---
+++ packages/openocd-esp32/default.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  inputs,
+  namespace,
+  pkgs,
+  stdenv,
+  fetchurl,
+  fetchzip,
+  writeShellScriptBin,
+  autoPatchelfHook,
+  libusb1,
+  zlib,
+  buildFHSUserEnv,
+}: let
+  name = "openocd-esp32";
+  owner = "espressif";
+  version = "0.11.0-esp32-20220706";
+  hash = "sha256-2t9+XRU3BT2QUSGqUBKjvozIHruFk0RTxIwRs9dPbG4=";
+  arch = "amd64";
+in
+  stdenv.mkDerivation rec {
+    inherit name version;
+
+    src = fetchzip {
+      url = "https://github.com/${owner}/${name}/releases/download/v${version}/${name}-linux-${arch}-${version}.tar.gz";
+      inherit hash;
+    };
+
+    buildInputs = [
+      zlib
+      libusb1
+    ];
+    nativeBuildInputs = [autoPatchelfHook];
+
+    phases = ["unpackPhase" "installPhase"];
+
+    installPhase = let
+      env = buildFHSUserEnv {
+        name = "esp32-openocd-env";
+        targetPkgs = pkgs: with pkgs; [zlib libusb1];
+        runScript = "";
+      };
+    in ''
+      cp -r . $out
+      autoPatchelf $out/bin/openocd
+    '';
+
+    passthru.update = writeShellScriptBin "update-my-package" ''
+      set -euo pipefail
+
+      latest="$(${pkgs.curl}/bin/curl -s "https://api.github.com/repos/${src.owner}/${src.repo}/releases?per_page=1" | ${pkgs.jq}/bin/jq -r ".[0].tag_name" | ${pkgs.gnused}/bin/sed 's/^v//')"
+
+      drift rewrite --auto-hash --new-version "$latest"
+    '';
+
+    meta = with lib; {
+      description = "ESP32 toolchain";
+      homepage = https://docs.espressif.com/projects/esp-idf/en/stable/get-started/linux-setup.html;
+      mainProgram = "openocd";
+      license = licenses.gpl3;
+    };
+  }
