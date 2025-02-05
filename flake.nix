266975ccddd5fae86cacfc94c6b813cdcac7851f
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-05 22:15:50 +0000

    feat: Add cache to Flake
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -1,4 +1,14 @@
 {
+  nixConfig = {
+    substituters = [
+      "https://esp32-dev-nix.cachix.org?priority=10"
+      "https://cache.nixos.org?priority=15"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "esp32-dev-nix.cachix.org-1:m6Jnzg6P2tl5KwKhKUByqq0NGr9E6htFmxijgI1Es2U="
+    ];
+  };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
     unstable.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
