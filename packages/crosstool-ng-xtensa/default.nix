b87995a39f248a31b41dab573a338e373fcee60d
Dom Rodriguez <shymega@shymega.org.uk>
2025-11-16 19:50:02 +0000

    refactor: Finish getting `crosstool-ng` working

--- packages/crosstool-ng-xtensa/default.nix
+++ packages/crosstool-ng-xtensa/default.nix
@@ -6,55 +6,71 @@
   stdenv,
   writeShellScriptBin,
   fetchFromGitHub,
+  ...
 }:
-with pkgs; let
-  owner = "espressif";
-  repo = "crosstool-ng";
+stdenv.mkDerivation (finalAttrs: {
   pname = "crosstool-ng-xtensa";
   version = "esp-14.2.0_20241119";
-  sha256 = "hRTq5AMODVlRygriGymQQ547KnK5yW4tqxqttaE19S8=";
-in
-  stdenv.mkDerivation {
-    inherit pname version;
 
-    src = fetchFromGitHub {
-      inherit owner repo sha256;
-      rev = "refs/tags/${version}";
-      leaveDotGit = true;
-    };
+  src = fetchFromGitHub {
+    owner = "espressif";
+    repo = "crosstool-ng";
+    sha256 = "hRTq5AMODVlRygriGymQQ547KnK5yW4tqxqttaE19S8=";
+    rev = "refs/tags/${finalAttrs.version}";
+    leaveDotGit = true;
+  };
 
-    nativeBuildInputs = [
-      autoconf
-      automake
-      binutils
-      bison
-      flex
-      gettext
-      git
-      gperf
-      help2man
-      libtool
-      ncurses
-      texinfo
-      which
-      wget
-      unzip
-    ];
+  nativeBuildInputs = with pkgs; [
+    autoconf
+    automake
+    coreutils
+    curl
+    gcc
+    git
+    python3
+  ];
 
-    preConfigure = "bash ./bootstrap";
+  buildInputs = with pkgs; [
+    aria
+    bison
+    cvs
+    file
+    flex
+    gperf
+    help2man
+    libtool
+    ncurses
+    texinfo
+    unzip
+    wget
+    which
+  ];
 
-    passthru.update = let
-      inherit (lib) getExe;
-      curl = getExe pkgs.curl;
-      jq = getExe pkgs.jq;
-    in
-      writeShellScriptBin "update-my-package" ''
-        set -euo pipefail
+  configurePhase = ''
+    ./bootstrap
+    ./configure --disable-static --enable-local --prefix=/usr
+  '';
 
-        latest="$(${curl} -s "https://api.github.com/repos/${owner}/${repo}/releases?per_page=1" | ${jq} -r ".[0].tag_name")"
+  buildPhase = ''
+    make
+  '';
 
-        drift rewrite --auto-hash --new-version "$latest"
-      '';
+  installPhase = ''
+    make DESTDIR=$out install
+  '';
 
-    meta.mainProgram = "ct-ng";
-  }
+  passthru.update = let
+    inherit (lib) getExe;
+    curl = getExe pkgs.curl;
+    jq = getExe pkgs.jq;
+  in
+    writeShellScriptBin "update-my-package" ''
+      set -euo pipefail
+
+      latest="$(${curl} -s "https://api.github.com/repos/${finalAttrs.src.owner}/${finalAttrs.src.repo}/releases?per_page=1" | ${jq} -r ".[0].tag_name")"
+
+      drift rewrite --auto-hash --new-version "$latest"
+    '';
+
+  meta.mainProgram = "ct-ng";
+})
