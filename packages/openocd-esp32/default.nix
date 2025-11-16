c85d2180005f6bd516bbbb1cb2bcbb7814ecbbe4
Dom Rodriguez <shymega@shymega.org.uk>
2025-11-16 20:32:14 +0000

    refactor: Update `openocd-esp32` to build from source

--- packages/openocd-esp32/default.nix
+++ packages/openocd-esp32/default.nix
@@ -1,62 +1,18 @@
-{
-  lib,
-  inputs,
-  namespace,
-  pkgs,
-  stdenv,
-  fetchurl,
-  fetchzip,
-  writeShellScriptBin,
-  autoPatchelfHook,
-  libusb1,
-  zlib,
-  buildFHSUserEnv,
-}: let
-  name = "openocd-esp32";
-  owner = "espressif";
-  version = "0.12.0-esp32-20241016";
-  sha256 = "sroUQw8yfAidKPk0oCoprEGqMnCJZwHrrvH9pAOITTI=";
-  arch = "amd64";
-in
-  stdenv.mkDerivation rec {
-    inherit name version;
+{ zlib, autoreconfHook, openocd, fetchFromGitHub, ... }:
+openocd.overrideAttrs (finalAttrs: prevAttrs: {
+  version = "0.12.0-esp32-20250707";
+  src = fetchFromGitHub {
+    repo = "openocd-esp32";
+    owner = "espressif";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-Tg/8Je+ixp4XL+y7ZLtMQ7U6umjUBqdPlXTuWLx54P4=";
+  };
 
-    src = fetchzip {
-      url = "https://github.com/${owner}/${name}/releases/download/v${version}/${name}-linux-${arch}-${version}.tar.gz";
-      inherit sha256;
-    };
+  nativeBuildInputs = (prevAttrs.nativeBuildInputs or []) ++ [
+    autoreconfHook
+  ];
 
-    buildInputs = [
-      zlib
-      libusb1
-    ];
-    nativeBuildInputs = [autoPatchelfHook];
-
-    phases = ["unpackPhase" "installPhase"];
-
-    installPhase = ''
-      cp -r . $out
-      autoPatchelf $out/bin/openocd
-    '';
-
-    passthru.update = let
-      inherit (lib) getExe;
-      curl = getExe pkgs.curl;
-      jq = getExe pkgs.jq;
-      sed = getExe pkgs.gnused;
-    in
-      writeShellScriptBin "update-my-package" ''
-        set -euo pipefail
-
-        latest="$(${curl} -s "https://api.github.com/repos/${owner}/${name}/releases?per_page=1" | ${jq} -r ".[0].tag_name" | ${sed} 's/^v//')"
-
-        drift rewrite --auto-hash --new-version "$latest"
-      '';
-
-    meta = with lib; {
-      description = "ESP32 toolchain";
-      homepage = https://docs.espressif.com/projects/esp-idf/en/stable/get-started/linux-setup.html;
-      mainProgram = "openocd";
-      license = licenses.gpl3;
-    };
-  }
+  buildInputs = (prevAttrs.buildInputs or []) ++ [
+    zlib
+  ];
+})
